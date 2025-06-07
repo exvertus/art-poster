160abbd6d8978cd288d06226df970b4cbb4b5c76
Thomas Flanigan <tflanig1@gmail.com>
2025-06-06 21:03:08 -0400

    Test cleanup: delete post after test

--- tests/test_bluesky.py
+++ tests/test_bluesky.py
@@ -44,24 +44,32 @@ def decode_qr_from_bytes(image_bytes):
         raise ValueError("QR image failed decoding")
     return decoded[0].data.decode()
 
-def test_img_post(test_client, random_qr_code):
-    original_number = random_qr_code["number"]
-    image = random_qr_code["image"]
-    image_bytes = random_qr_code["bytes"]
-
-    post_response = bluesky.img_post(image, 
-                                     image_bytes, 
+@pytest.fixture
+def bluesky_post(test_client, random_qr_code):
+    post_response = bluesky.img_post(random_qr_code["image"], 
+                                     random_qr_code["bytes"], 
                                      caption='This is a test.',
                                      client=test_client)
-
     post_rkey = post_response.uri.split('/')[-1]
-    get_response = test_client.get_post(post_rkey)
+
+    yield {
+        "post_rkey": post_rkey,
+        "qr_number": random_qr_code["number"],
+        "image_ref": post_response
+    }
+
+    test_client.delete_post(post_response.uri)
+
+def test_img_post(test_client, bluesky_post):
+    original_qr_num = bluesky_post["qr_number"]
+
+    get_response = test_client.get_post(bluesky_post["post_rkey"])
     response_image = get_response.value.embed.images[0]
     blob_ref = response_image.image.ref.link
     repo = test_client.me.did
     blob_bytes = test_client.com.atproto.sync.get_blob(
         params={"did": repo, "cid": blob_ref}
     )
-    decoded_number = decode_qr_from_bytes(blob_bytes)
+    decoded_qr_num = decode_qr_from_bytes(blob_bytes)
 
-    assert decoded_number == original_number
+    assert decoded_qr_num == original_qr_num
